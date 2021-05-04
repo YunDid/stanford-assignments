1a86c1bb8a61ca397ef5738f3a43b162973b09da
YunDid <512862613@qq.com>
2021-05-04 11:02:10 +0800

    Assign0-LifeGame

--- Assignment_Life/Life/src/life.cpp
+++ Assignment_Life/Life/src/life.cpp
@@ -28,7 +28,7 @@ void welcome_Messages();
 void input_File(string, ifstream&);
 void colony_Initializer(Grid<char>&, Grid<char>&, int&, int&, ifstream&, LifeGUI&);
 void menu(char&, Grid<char>&, Grid<char>&, bool, LifeGUI&);
-bool wrapping_Indicator();
+bool wrapping_Indicator(Grid<char>&);
 
 ///* Sub-functions */
 void display_All_Colony(const Grid<char>&);
@@ -58,8 +58,8 @@ int main() {
     colony_Initializer(currColony, nextColony, row, col, fin, gui);
 
     /* Menu */
-//    char option;        // menu option
-//    menu(option, currColony, nextColony, wrapping_Indicator(), gui);
+    char option;        // menu option
+    menu(option, currColony, nextColony, wrapping_Indicator(currColony), gui);
 
     /* Ending */
     cout << "Have a nice Life!" << endl;
@@ -83,39 +83,26 @@ void welcome_Messages() {
 // Input text file
 void input_File(string filePath, ifstream& fin) {
     while(true) {
-        string filename = getLine("Grid input file name? \n");
+        string filename = getLine("Grid input file name? ");
         filePath = "res/" + filename + ".txt";
         if(!fileExists(filePath)) {
             cout << "Can't locate the file, try again.\n" <<endl;
         }else {
             openFile(fin, filePath);
-            string value1,value2;
-            fin>>value1>>value2;
-            fin.seekg(0, ios::beg);  //文件指针重定位到文件首部 否则因为是引用修改了指针位置
-            cout<<value1<<endl<<value2<<endl;
+//            string value1,value2;
+//            fin>>value1>>value2;
+//            fin.seekg(0, ios::beg);  //文件指针重定位到文件首部 否则因为是引用修改了指针位置
+//            cout<<value1<<endl<<value2<<endl;
             break;
         }
     }
-
-//    while(true) {
-//        filePath = getLine("Grid input file name? ");
-//        if (!fileExists(filePath)) {
-//            cout << "Can't locate the file, try again.\n";
-//        } else {
-//            openFile(fin, filePath);
-//            break;
-//        }
-//    }
 }
 
 
 void colony_Initializer(Grid<char>& currColony, Grid<char>& nextColony, int& row, int& col, ifstream& fin, LifeGUI& gui) {
     // Read in data from file:
-    char r,c;
-    fin >> r;
-    fin >> c;
-    row = r - '0';
-    col = c - '0';
+
+    fin >> row >> col;
     currColony.resize(row, col);       // setup size for current generation grids
     nextColony.resize(row, col);       // setup size for next generation grids
     gui.resize(row, col);              // setup size for GUI window
@@ -131,74 +118,80 @@ void colony_Initializer(Grid<char>& currColony, Grid<char>& nextColony, int& row
 
     // Print grids for the first time:
     cout << endl;
-    display_All_Colony(currColony);
+//    display_All_Colony(currColony);
 }
 
-//// create annimation accroding to frame number
-//void colony_Animation(Grid<char>& currColony, Grid<char>& nextColony, int frames, bool wrapping, LifeGUI& gui) {
-//    for (int i = 0; i < frames; i++) {
-//        // how many frames would generate for the animation
-//        clearConsole();
-//        colony_Iteration(currColony, nextColony, wrapping, gui);
+// create annimation accroding to frame number
+void colony_Animation(Grid<char>& currColony, Grid<char>& nextColony, int frames, bool wrapping, LifeGUI& gui) {
+    for (int i = 0; i < frames; i++) {
+        // how many frames would generate for the animation
+        clearConsole();
+        colony_Iteration(currColony, nextColony, wrapping, gui);
 //        currColony.deepCopy(nextColony);
-//        pause(50); // pause time
-//    }
-//}
+        for (int i = 0; i < nextColony.numRows(); i++) {
+            for(int j = 0; j < nextColony.numCols(); j++){
+               currColony[i][j] =nextColony[i][j];
+           }
+        }
 
-//// main menu
-//void menu(char& option, Grid<char>& currColony, Grid<char>& nextColony, bool wrapping, LifeGUI& gui) {
-//    int frames; // for how many new generations are shown
+        pause(50); // pause time
+    }
+}
 
-//    cout << "\nA)inmate, T)ick, Q)uit? ";
-//    cin >> option;
-//    // input validation
-//    while (!cin || (cin.peek() != '\n')) {
-//        cout << "Invalid input, try again.\n";
-//        cin.clear();
-//        cin.ignore(999, '\n');
-//        cout << "\nA)inmate, T)ick, Q)uit? ";
-//        cin >> option;
-//    }
+// main menu
+void menu(char& option, Grid<char>& currColony, Grid<char>& nextColony, bool wrapping, LifeGUI& gui) {
+    int frames; // for how many new generations are shown
 
-//    switch (option) {
-//    case 'a':
-//    case 'A':
-//        // animation option:
-//        cout << "How many frames? ";
-//        cin >> frames;
-//        // input validation
-//        while (!cin || (cin.peek() != '\n')) {
-//            cout << "Invalid input, try again.\n";
-//            cin.clear();
-//            cin.ignore(999, '\n');
-//            cout << "How many frames? ";
-//            cin >> frames;
-//        }
-//        colony_Animation(currColony, nextColony, frames, wrapping, gui);
-//        menu(option, currColony, nextColony, wrapping, gui);
-//        break;
+    cout << "\nA)inmate, T)ick, Q)uit? ";
+    cin >> option;
+    // input validation
+    while (!cin || (cin.peek() != '\n')) {
+        cout << "Invalid input, try again.\n";
+        cin.clear();
+        cin.ignore(999, '\n');
+        cout << "\nA)inmate, T)ick, Q)uit? ";
+        cin >> option;
+    }
 
-//    case 't':
-//    case 'T':
-//        // tick option:
-//        colony_Animation(currColony, nextColony, 1, wrapping, gui);
-//        menu(option, currColony, nextColony, wrapping, gui);
-//        break;
+    switch (option) {
+    case 'a':
+    case 'A':
+        // animation option:
+        cout << "How many frames? ";
+        cin >> frames;
+        // input validation
+        while (!cin || (cin.peek() != '\n')) {
+            cout << "Invalid input, try again.\n";
+            cin.clear();
+            cin.ignore(999, '\n');
+            cout << "How many frames? ";
+            cin >> frames;
+        }
+        colony_Animation(currColony, nextColony, frames, wrapping, gui);
+        menu(option, currColony, nextColony, wrapping, gui);
+        break;
 
-//    case 'q':
-//    case 'Q':
-//        // quit program
-//        break;
+    case 't':
+    case 'T':
+        // tick option:
+        colony_Animation(currColony, nextColony, 1, wrapping, gui);
+        menu(option, currColony, nextColony, wrapping, gui);
+        break;
 
-//    default:
-//        // otherwise prompt again:
-//        cin.clear();
-//        cin.ignore(999, '\n');
-//        cout << "Invalid input, Try again.";
-//        menu(option, currColony, nextColony, wrapping, gui);
-//        break;
-//    }
-//}
+    case 'q':
+    case 'Q':
+        // quit program
+        break;
+
+    default:
+        // otherwise prompt again:
+        cin.clear(); //更改cin的状态标识符 使其恢复使用
+        cin.ignore(999, '\n'); //把回车（包括回车）之前的所以字符从输入缓冲（流）中清除出去。
+        cout << "Invalid input, Try again.";
+        menu(option, currColony, nextColony, wrapping, gui);
+        break;
+    }
+}
 
 //// print colony
 void display_All_Colony(const Grid<char>& g) {
@@ -206,94 +199,95 @@ void display_All_Colony(const Grid<char>& g) {
         for(int j = 0; j < g.numCols(); j++){
             cout << g[i][j];
        }
+        if(i!=g.numRows()-1)
         cout << endl;
     }
 }
 
-//// non-wrapping condition
-//int neighbourNum_NonWrap(const Grid<char>& g, int r, int c) {
-//    int neighbour = 0;
+// non-wrapping condition
+int neighbourNum_NonWrap(const Grid<char>& g, int r, int c) {
+    int neighbour = 0;
 
-//    // count neighbours around each cell's 8 directioins
-//    for (int i = -1; i < 2; i++) {
-//        for (int j = -1; j < 2; j++) {
-//            if (g.inBounds(r + i, c + j) && g[r + i][c + j] == 'X') {
-//                neighbour++;
-//            }
-//        }
-//    }
+    // count neighbours around each cell's 8 directioins
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            if (g.inBounds(r + i, c + j) && g[r + i][c + j] == 'X') {
+                neighbour++;
+            }
+        }
+    }
 
-//    // do not count itself if cell exists
-//    if (g[r][c] == 'X') {
-//        neighbour--;
-//    }
+    // do not count itself if cell exists
+    if (g[r][c] == 'X') {
+        neighbour--;
+    }
 
-//    return neighbour;
-//}
+    return neighbour;
+}
 
-//// wrapping condition
-//int neighbourNum_Wrap(const Grid<char>& g, int r, int c) {
-//    int neighbour = 0;
+// wrapping condition
+int neighbourNum_Wrap(const Grid<char>& g, int r, int c) {
+    int neighbour = 0;
 
-//    // count neighbours around each cell's 8 directioins
-//    for (int i = -1; i < 2; i++) {
-//        for (int j = -1; j < 2; j++) {
-//            if (g[(r+i+g.numRows()) % g.numRows()][(c+j+g.numCols()) % g.numCols()] == 'X') {
-//                neighbour++;
-//            }
-//        }
-//    }
+    // count neighbours around each cell's 8 directioins
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            if (g[(r+i+g.numRows()) % g.numRows()][(c+j+g.numCols()) % g.numCols()] == 'X') {
+                neighbour++;
+            }
+        }
+    }
 
-//    // do not count itself if cell exists
-//    if (g[r][c] == 'X') {
-//        neighbour--;
-//    }
+    // do not count itself if cell exists
+    if (g[r][c] == 'X') {
+        neighbour--;
+    }
 
-//    return neighbour;
-//}
+    return neighbour;
+}
 
-//// generation iteration: each function call will update the whole grid by 1.
-//void colony_Iteration(Grid<char>& currColony, Grid<char>& nextColony, bool wrapping, LifeGUI& gui) {
-//    int neigbourNumber;  // number of neigbour for each cell
+// generation iteration: each function call will update the whole grid by 1.
+void colony_Iteration(Grid<char>& currColony, Grid<char>& nextColony, bool wrapping, LifeGUI& gui) {
+    int neigbourNumber;  // number of neigbour for each cell
 
-//    // loop over each grid in the colony
-//    for (int i = 0; i < currColony.numRows(); i++) {
-//        // for each row
-//        for (int j = 0; j < currColony.numCols(); j++) {
-//            // for each grid
+    // loop over each grid in the colony
+    for (int i = 0; i < currColony.numRows(); i++) {
+        // for each row
+        for (int j = 0; j < currColony.numCols(); j++) {
+            // for each grid
 
-//            // eable wrapping or not
-//            if (wrapping) {
-//                neigbourNumber = neighbourNum_Wrap(currColony, i, j);
-//            } else {
-//                neigbourNumber = neighbourNum_NonWrap(currColony, i, j);
-//            }
+            // eable wrapping or not
+            if (wrapping) {
+                neigbourNumber = neighbourNum_Wrap(currColony, i, j);
+            } else {
+                neigbourNumber = neighbourNum_NonWrap(currColony, i, j);
+            }
 
-//            // apply rules of game of life:
-//            if (neigbourNumber <= 1 || neigbourNumber >= 4) { // when neigbour # >= 4 or <= 1, die;
-//                nextColony.set(i, j, '-');
-//                gui.drawCell(i, j, false);
-//            } else if (neigbourNumber == 3) { // when neigbour # == 3, live;
-//                nextColony.set(i, j, 'X');
-//                gui.drawCell(i, j, true);
-//            }
+            // apply rules of game of life:
+            if (neigbourNumber <= 1 || neigbourNumber >= 4) { // when neigbour # >= 4 or <= 1, die;
+                nextColony.set(i, j, '-');
+                gui.drawCell(i, j, false);
+            } else if (neigbourNumber == 3) { // when neigbour # == 3, live;
+                nextColony.set(i, j, 'X');
+                gui.drawCell(i, j, true);
+            }
 
-//        }
-//    }
+        }
+    }
 
-//    // after update, print colony
-//    display_All_Colony(nextColony);
-//}
+    // after update, print colony
+    display_All_Colony(nextColony);
+}
 
 
-//// Prompt user for wrapping condition:
-//bool wrapping_Indicator() {
-//    char wrapOption;
+// Prompt user for wrapping condition:
+bool wrapping_Indicator(Grid<char>& currColony) {
+    char wrapOption;
 
-//    cout << "Should the simulation wrap around the grid (y/n)? ";
-//    cin >> wrapOption;
+    cout << "Should the simulation wrap around the grid (y/n)? ";
+    cin >> wrapOption;
 
-//    // input validation
+    // input validation
 //    while(!cin || cin.peek() != '\n') {
 //        cout << "Invalid input, try again.\n";
 //        cin.clear();
@@ -302,23 +296,25 @@ void display_All_Colony(const Grid<char>& g) {
 //        cin >> wrapOption;
 //    }
 
-//    switch (wrapOption) {
-//    case 'y':
-//    case 'Y':
-//        return true;
-//        break;
-//    case 'n':
-//    case 'N':
-//        return false;
-//        break;
-//    default:
-//        cin.clear();
-//        cin.ignore(9999, '\n');
-//        cout << "Invalid input, try again. \n";
-//        return wrapping_Indicator();
-//        break;
-//    }
-//}
+    switch (wrapOption) {
+    case 'y':
+    case 'Y':
+        display_All_Colony(currColony);
+        return true;
+        break;
+    case 'n':
+    case 'N':
+        display_All_Colony(currColony);
+        return false;
+        break;
+    default:
+        cin.clear();
+        cin.ignore(9999, '\n');
+        cout << "Invalid input, try again. \n";
+        return wrapping_Indicator(currColony);
+        break;
+    }
+}
 
 // read in colony grids
 void read_Colony(Grid<char>& g, ifstream& fin, LifeGUI& gui) {
@@ -331,5 +327,5 @@ void read_Colony(Grid<char>& g, ifstream& fin, LifeGUI& gui) {
 
        }
     }
-    fin.seekg(0, ios::beg);
+//    fin.seekg(0, ios::beg);
 }
